82962d4637e8ea6ef45cf4a063955d63c31abd89
zac-roland <zacgilliland@gmail.com>
2021-12-05 21:12:51 -0500

    Add boilerplate style. Add components dir and Navigation component for header. Remove create-react-app App component. Tutorial https://www.techomoro.com/how-to-create-a-multi-page-website-with-react-in-5-minutes/

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Navigation } from './components';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Navigation />
+  </Router>,
   document.getElementById('root')
 );
 
